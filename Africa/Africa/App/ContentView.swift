5be9b79b127cf3bb4615e08b982d56d5aeb2406b
Clément Causer <ccauser@simplonprod.co>
2022-05-05 12:33:36 +0200

    add detail screen part 1

--- Africa/Africa/App/ContentView.swift
+++ Africa/Africa/App/ContentView.swift
@@ -11,20 +11,22 @@ struct ContentView: View {
     var body: some View {
         // MARK: - PROPERTIES
         let animals: [Animal] = Bundle.main.decode("animals.json")
-        
+
         // MARK: - BODY
         NavigationView {
             List {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
+
                 ForEach(animals) { animal in
-                    AnimalListItemView(animal: animal)
+                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                        AnimalListItemView(animal: animal)
+                    }
                 }
             } //: LIST
             .navigationTitle("Africa")
-            .navigationBarTitleDisplayMode(.large)
+                .navigationBarTitleDisplayMode(.large)
         } //: NAVIGATION
     }
 }
