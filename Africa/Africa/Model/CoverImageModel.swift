5efcea889f2c4f3e842bd33eeb39c09574e21b3b
Clément Causer <ccauser@simplonprod.co>
2022-05-05 11:57:18 +0200

    add json decode extension + heading carousel

---
+++ Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Clément Causer on 05/05/2022.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
